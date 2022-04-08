3ec848c3eee1d788cbb3cdad510e07d46c6455a3
juanesleal <47696560+juanesleal@users.noreply.github.com>
2022-04-08 00:56:14 -0400

    floor update with new communication

--- src/main/Floor.java
+++ src/main/Floor.java
@@ -1,16 +1,28 @@
 package main;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.*;
 import java.time.Clock;
 import java.util.LinkedList;
+import java.util.concurrent.TimeUnit;
 
 public class Floor {
 
     private Communicator floorCommunicator;
+    private Communicator outputCommunicator;
     private LinkedList<String[]> messages = new LinkedList<>();
     private int nextFloorReq = 0;
+    
+    private DatagramPacket sendPacket;
+	private DatagramSocket socket;
+	private ByteArrayOutputStream buildPacket;
+	private String mode = "netASCII";
+	private String filename = "test.txt";
+	private InetAddress localHostVar;
+
 
     /**
      * Generates a new floor subsystem that communicates using the specified EventHolder
@@ -19,37 +31,26 @@ public class Floor {
     public Floor() {
         Communicator com = new Communicator();
         floorCommunicator = new Communicator(com.FLOOR_PORT, "Floor");
+        outputCommunicator = new Communicator(com.OUTPUT_PORT, "Output");
     }
+    
 
     public static void main(String[] args) {
         Clock time = Clock.systemDefaultZone();
         Floor f = new Floor();
         f.readAndSort();
-        while (true) {
-            Message m = f.floorCommunicator.receive(0);
-            if (m.getData()[0].equals("EventReq")) {
-                //scheduler wants next event, we've already sorted the events...
-                f.floorCommunicator.send(new Message(f.messages.get(f.nextFloorReq), time.millis(), m.getToFrom()));
-                f.nextFloorReq++;
-            }
-            if (m.getData()[0].equals("ButtonReq")) {
-                boolean ok = false;
-                //figure out which floor it's for...
-                for (String[] s : f.messages) {
-                    if (m.getData()[1].equals(s[1])) {
-                        f.floorCommunicator.send(new Message(new String[]{"OK", s[4]}, time.millis(), m.getToFrom()));
-                        ok = true;
-                    }
-                }
-                if (!ok) {
-                    f.floorCommunicator.send(new Message(new String[]{"Request Not Availible"}, time.millis(), m.getToFrom()));
-                }
-            }
+        Message m = f.floorCommunicator.receive(0);
+       
+        //edited portion
+        while(true) {
+        		
+        			floorMessageCheck(f);
+        		
+        		}
         }
         // when floor gets a request send in the following format: "FloorRequest, time, where, Up/Down, buttonReq on arrival"
 
         //System.out.println("== Floor Subsystem finished");
-    }
     private void readAndSort() {
         try {
             //    Read floor data values from file
@@ -97,4 +98,53 @@ public class Floor {
             System.err.println(e.getMessage());
         }
     }
+    
+    private static void floorMessageCheck(Floor floor) {
+    	 Clock time = Clock.systemDefaultZone();
+         floor.readAndSort();
+         Message m = floor.floorCommunicator.receive(0);
+
+         	if (!m.getData()[0].equals("OK")) {
+         		
+
+ 		            if (m.getData()[0].equals("ButtonReq")) {
+ 		                boolean ok = false;
+ 		                //figure out which floor it's for...
+ 		                for (String[] s : floor.messages) {
+ 		                    if (m.getData()[1].equals(s[1])) {
+ 		                        floor.floorCommunicator.send(new Message(new String[]{"OK", s[4]}, time.millis(), m.getToFrom()));
+ 		                        ok = true;
+ 		                    }
+ 		                }
+ 		                if (!ok) {
+ 		                    floor.floorCommunicator.send(new Message(new String[]{"Request Not Availible"}, time.millis(), m.getToFrom()));
+ 		                }
+ 		              floor.floorCommunicator.send(new Message(new String[]{"idk if this works"}, time.millis(), "Scheduler"));
+
+ 	            }
+ 		            
+ 		            
+         	}
+         	else {		
+		            if (m.getData()[0].equals("EventReq")) {
+		                //scheduler wants next event, we've already sorted the events...
+		                floor.floorCommunicator.send(new Message(floor.messages.get(floor.nextFloorReq), time.millis(), m.getToFrom()));
+		                floor.nextFloorReq++;
+		            }
+		            else {
+		            	try {
+							TimeUnit.SECONDS.sleep(1);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+		            	floorMessageCheck(floor);
+		            }
+         	
+         	}
+
+    }
+    
+    
+    
 }
