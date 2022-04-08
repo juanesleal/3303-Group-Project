9c3fc2973f52e75d6a02c88b2f2f947fe3789c38
Oluwaseyi Sehinde-Ibini <seyisehindeibini@cmail.carleton.ca>
2022-04-08 07:53:02 -0400

    output file changed a bit

--- src/main/Output.java
+++ src/main/Output.java
@@ -1,26 +1,24 @@
 package main;
 
 public class Output {
-	
-	Communicator com = new Communicator();
-	
-	private Communicator cCommunicator;
-    private Communicator dCommunicator = new Communicator(com.OUTPUT_PORT, "Output");
+	//declaring this here but should be in communicator class
+	final int OUTPUT_PORT = 35;
 
-    public static void main(String[] args) {
-		Output o = new Output();
-		while(true) {
 
+	private Communicator dCommunicator = new Communicator(com.OUTPUT_PORT, "output");
+
+	public Output()
+
+	{
+		while ( true) {
 			System.out.println("The output is being sent");
-
-			Message m = o.dCommunicator.receive(0);
-
+			Message m = dCommunicator.receive(0);
 			System.out.println("Output message is:" + m);
 
 			//should be a condition here to stop the communicator
 		}
 	}
 
-	
-	
+
 }
+
